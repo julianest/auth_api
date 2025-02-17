cc44eee51f4872b04350e3ecba98f3eb0f6452c0
Charlinson <charlinson2013@gmail.com>
2025-02-17 11:07:45 -0500

    chore: elimina importaciones y código comentado

--- src/main/java/com/users/auth_api/config/SecurityConfig.java
+++ src/main/java/com/users/auth_api/config/SecurityConfig.java
@@ -11,13 +11,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.users.auth_api.component.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -58,17 +54,4 @@ public class SecurityConfig implements WebMvcConfigurer {
         return http.build();
     }
 
-
-    /*@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }*/
 }
